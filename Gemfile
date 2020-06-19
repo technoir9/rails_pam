34695fa27426e5cf3ea3a9e01cb78e669bedfbc4
technoir9 <vimes9@gmail.com>
2020-06-20 00:12:25 +0200

    Add PAM integration

--- Gemfile
+++ Gemfile
@@ -55,3 +55,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Use devise for user authentication
 gem 'devise'
+
+# Use devise_pam_authenticatable2 for PAM integration
+gem 'devise_pam_authenticatable2', '~> 9.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :pam_authenticatable
+
+  attr_accessor :password
+  @password = nil
+
+  def password_required?
+    false
+  end
 end
